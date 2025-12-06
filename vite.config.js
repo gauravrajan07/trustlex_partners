27114775bcfe196a39afbb08844e86dd6c5ac1e7
DevanshuSinghai <devanshusinghai2005@gmail.com>
2025-12-06 17:02:39 +0530

    updatd meta image

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/advTejasviGoel/",
+  base: "/advtejasvigoel/",
   plugins: [react()],
 })
